7b5b34070c4186de1ebf4d39ea9ac3d0d56a2b61
George Villasboas <gvillasboas@apple.com>
2016-10-05 11:47:23 -0300

    Replacing open_uri by open_uri_redirections to allow http->https safe redirections
    This gem contains outdated URLs for the sources it relies to pull data from.
    These outdated URLs are unsafe and wikipedia started to adopt https by default.
    The http gem used in this project (open_uri) doesn't support redirects. open_uri_redirections patches that.

--- lib/codename/lister/stars_lister.rb
+++ lib/codename/lister/stars_lister.rb
@@ -1,13 +1,13 @@
 require 'rubygems'
 require 'nokogiri'
-require 'open-uri'
+require 'open_uri_redirections'
 
 module Codename::Lister
   class StarsLister < BaseLister
     URL = "http://en.wikipedia.org/wiki/List_of_constellations"
 
     def list
-      doc = Nokogiri::HTML(open(URL))
+      doc = Nokogiri::HTML(open(URL, :allow_redirections => :safe))
       doc.css("table.wikitable tr > td[1] > a[1]").collect { |node| node.text }
     end
     
